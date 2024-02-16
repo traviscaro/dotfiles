28634921ca78f9e332f6300ba95279c4ab8571a8
Travis Caro <travisbcaro@gmail.com>
2024-02-15 21:51:08 -0500

    update dotfiles

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -680,9 +680,11 @@ cmp.setup {
   },
 }
 
+-- Setup Copilot for neovim. Firstly, Copilot clashes on the <Tab> binding with nvim-cmp, so we will remap it to <C-J>.
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
+-- Set the filetypes that Copilot will be enabled for.
 vim.g.copilot_filetypes = {
   ["*"] = false,
   ["markdown"] = true,
@@ -699,4 +701,3 @@ vim.g.copilot_filetypes = {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
